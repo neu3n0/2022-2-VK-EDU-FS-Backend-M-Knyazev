4fc80829a3db74d771521312850ed70ca468190d
neu3n0 <neu3n0.sH@gmail.com>
2022-11-01 17:01:32 +0300

    add members to chat

--- messenger/chats/admin.py
+++ messenger/chats/admin.py
@@ -3,5 +3,6 @@ from .models import Chat
 
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
+    filter_horizontal = ('members',)
     list_display = ('id', 'title', 'description', 'created_at', 'creator')
     model = Chat
