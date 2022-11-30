9364fdb1f7f2dac39e281395081e912addf38906
neu3n0 <neu3n0.sH@gmail.com>
2022-11-30 19:11:53 +0300

    dz10)))

--- messenger/messageschat/serializers.py
+++ messenger/messageschat/serializers.py
@@ -1,10 +1,35 @@
 from rest_framework import serializers
-
+from django.db import transaction
 from .models import Message
+from bs4 import BeautifulSoup
 
 
 class MessageCreateSerializer(serializers.ModelSerializer):
     """Serializer for create message"""
+
+    @transaction.atomic
+    def save(self, **kwargs):
+        t = self.validated_data['text']
+        soup = BeautifulSoup(t, 'html.parser')
+
+        # 1
+        # if soup.get_text() != t:
+        #     self.validated_data['text'] = 'бэд хацкер))))))'
+
+        # 2
+        # for s in soup.select('script, img'):
+        #     s.extract()
+        # self.validated_data['text'] = str(soup)
+
+        # 3
+        invalid_tags = ['script', 'img']
+        for tag in invalid_tags:
+            for match in soup.findAll(tag):
+                match.replaceWithChildren()
+
+        self.validated_data['text'] = str(soup)
+
+        super().save(**kwargs)
 
     class Meta:
         model = Message
